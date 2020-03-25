af4c49162815ce612fcb15b74ac962236ed446bf
Ronnie <rlrodrig88@gmail.com>
2020-03-25 12:32:05 -0500

    Basic Authentication

--- app.js
+++ app.js
@@ -37,6 +37,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Authentication
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error("You are not authenticated!");
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+  // get and parse the encoded username and password and put them in array
+  const auth = Buffer.from(authHeader.split(''), 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+    return next(); // authorized
+  } else {
+    const err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    return next(err);
+  }
+
+
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

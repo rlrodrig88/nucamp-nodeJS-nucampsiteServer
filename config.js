f6622ba5729ee8f0bd52cde64fef58792ae9d0db
Ronnie Rodriguez <rlrodrig88@gmail>
2020-04-01 22:14:43 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+    clientId: '1073863429665683',
+    clientSecret: '9e385a9012d2cc5eddf7dd413e9b633e'
+  }
 };
